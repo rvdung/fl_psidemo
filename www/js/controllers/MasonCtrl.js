d3b02e4aa5b6c7c4ed9e7c1460d9c8dad7be79e4
Tu Nguyen <nguyenphantu88@gmail.com>
2016-11-29 23:42:04 +0700

    chọn huyện, click to call

--- www/js/controllers/MasonCtrl.js
+++ www/js/controllers/MasonCtrl.js
@@ -1,17 +1,64 @@
-psiApp.controller('MasonCtrl', function($scope,$http,$ionicScrollDelegate) {
+psiApp.controller('MasonCtrl', function($scope,$http) {
 
-	initData();
+	const  caibe_json="resources/json/mason_caibe.json";
+	const  cailay_json="resources/json/mason_cailay.json";
+	const  chauthanh_json="resources/json/mason_chauthanh.json";
+	const  chogao_json="resources/json/mason_chogao.json";
+	const  gocong_json="resources/json/mason_gocong.json";
+	const  gocongtay_json="resources/json/mason_gocongtay.json";
+
+	$scope.huyens = [{code:"caibe", label:"Huyện Cái Bè"},{code:"cailay", label:"Huyện Cai Lậy"},
+         {code:"chauthanh", label:"Huyện Châu Thành"},
+         {code:"chogao", label:"Huyện Chợ Gạo"},
+         {code:"gocongtay", label:"Huyện Gò Công Tây"},
+         {code:"gocong", label:"Thị Xã Gò Công"} ];
+     $scope.selectHuyen = $scope.huyens[0];
 	
+	initData();
+
 	function initData() {
-		$http.get('resources/json/mason.json').success(function(response) {
+		$http.get(caibe_json).success(function(response) {
 			$scope.masons = response;
 		});
 	};
 
-	$scope.$watch('searchText',function(value){
-	    console.log(value);
-	  });
-  
-  	$ionicScrollDelegate.scrollTop(false);
-	
-});
+	$scope.call = function(tel) {
+		window.location.href = "tel:" + tel;
+	}
+
+	$scope.changeHuyen = function(value){
+		if("caibe" === value.code) {
+			$http.get(caibe_json).success(function(response) {
+				$scope.masons = response;			
+			});
+			return;
+		}
+		if("cailay" === value.code) {
+			$http.get(cailay_json).success(function(response) {
+				$scope.masons = response;			
+			});
+			return;
+		}
+		if("chogao" === value.code) {
+			$http.get(chogao_json).success(function(response) {
+				$scope.masons = response;			
+			});
+			return;
+		}
+		if("chauthanh" === value.code) {
+			$http.get(chauthanh_json).success(function(response) {
+				$scope.masons = response;			
+			});
+			return;
+		}
+		if("gocongtay" === value.code) {
+			$http.get(gocongtay_json).success(function(response) {
+				$scope.masons = response;			
+			});
+			return;
+		}
+		$http.get(gocong_json).success(function(response) {
+			$scope.masons = response;			
+		});
+	  };
+ });
