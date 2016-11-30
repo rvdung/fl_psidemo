762969fa08e0a67fa4041c20921a4e31d2ef2677
Tu Nguyen <nguyenphantu88@gmail.com>
2016-11-30 22:57:49 +0700

    thêm trang navigation mục ghi chú, thêm nút tạo event, sửa click to call để hiển thị đúng sđt

--- www/js/controllers/NotesNavigationCtrl.js
+++ www/js/controllers/NotesNavigationCtrl.js
@@ -1,4 +1,4 @@
-psiApp.controller('NotesNavigationCtrl', function($scope, $state) {
+psiApp.controller('NotesNavigationCtrl', function($scope, $state,$cordovaCalendar) {
 
 	$scope.menu = [{code: "ghichu",label: "Ghi chú", icon:"ion-clipboard"},
 	               {code: "lich",label: "Xem lịch", icon:"ion-ios-calendar"},
@@ -6,12 +6,21 @@ psiApp.controller('NotesNavigationCtrl', function($scope, $state) {
 
     $scope.getMenuAction = function(menuItem) {
         if (menuItem.code === 'ghichu') {
-        	alert('in');
             $state.go('tab.trackers');
         } else if (menuItem.code === 'lich') {
     		window.plugins.calendar.openCalendar();
         } else if (menuItem.code === 'nhacviec') {
-            $state.go('tab.event');
+              $cordovaCalendar.createEventInteractively({
+                title: 'tiêu đề',
+                location: 'địa điểm',
+                notes: 'nhắc viêc',
+                startDate: new Date(),
+                endDate: new Date()
+              }).then(function (result) {
+                // success
+              }, function (err) {
+                // error
+              });
         } else {
             $state.go('tab.notes-nav');
         }
