56b57b52c92cee102aa716b1b5beae467b9692f1
Tu Nguyen <nguyenphantu88@gmail.com>
2016-12-06 01:39:22 +0700

    xóa nhầm code của Dũng

--- www/js/controllers/SettingCtrl.js
+++ www/js/controllers/SettingCtrl.js
@@ -1,16 +1,19 @@
 psiApp.controller('SettingCtrl', function($scope, $rootScope, $state,ProvincesService,  ViewCountingService) {
 	
+	$scope.init = function() {
 		ViewCountingService.getViewCount(function(){
-			$scope.statystics = [];
+			$scope.statistics = [];
 			var i;
 			for(i=0; i < $rootScope.menu.length; i ++){
-				$scope.statystics.push({
+				alert($rootScope.menu[i].label);
+				$scope.statistics.push({
 					label: $rootScope.menu[i].label,
 					count: $rootScope.viewCount[$rootScope.province.provinceCode][$rootScope.menu[i].code]
 				});
 			}
 		});
 	};
+
 	
 	$scope.clearProvince = function() {
 		$rootScope.province = undefined;
@@ -22,7 +25,6 @@ psiApp.controller('SettingCtrl', function($scope, $rootScope, $state,ProvincesSe
 		$state.go('tab.home');
 	} else {
 		$scope.init();
-	}
-	
+	}	
 	
 });
