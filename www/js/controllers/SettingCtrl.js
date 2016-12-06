db9d3544970ac0dfda321de9367edb9c7ed4eb2e
nvdung <nvdung@nvdung.IfiSolution.local>
2016-12-06 11:24:30 +0700

    fix Tú

--- www/js/controllers/SettingCtrl.js
+++ www/js/controllers/SettingCtrl.js
@@ -1,11 +1,10 @@
-psiApp.controller('SettingCtrl', function($scope, $rootScope, $state,ProvincesService,  ViewCountingService) {
+psiApp.controller('SettingCtrl', function($scope, $rootScope, $state,  ViewCountingService) {
 	
 	$scope.init = function() {
 		ViewCountingService.getViewCount(function(){
 			$scope.statistics = [];
 			var i;
 			for(i=0; i < $rootScope.menu.length; i ++){
-				alert($rootScope.menu[i].label);
 				$scope.statistics.push({
 					label: $rootScope.menu[i].label,
 					count: $rootScope.viewCount[$rootScope.province.provinceCode][$rootScope.menu[i].code]
@@ -13,18 +12,15 @@ psiApp.controller('SettingCtrl', function($scope, $rootScope, $state,ProvincesSe
 			}
 		});
 	};
-
 	
 	$scope.clearProvince = function() {
 		$rootScope.province = undefined;
 		$state.go('login');
 	}
 	
-
 	if ($rootScope.menu == undefined) {
 		$state.go('tab.home');
 	} else {
 		$scope.init();
-	}	
-	
+	}
 });
